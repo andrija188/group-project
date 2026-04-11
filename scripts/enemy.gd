7736f27b76998b2d4dc4cedce8c43116a80a76f4
kosta222 <dimitrijevickosta2011@gmail.com>
2026-04-11 20:28:40 +0200

    main

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-@onready var player = $Player
+@onready var player = get_tree().get_first_node_in_group("player")
 var speed: float = 1000.0
 var follow = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body == player:
+	if body.is_in_group("player"):
 		follow = true
+		print("TOUCHED:", body.name)
 
 func _physics_process(delta):
-	if follow:
+	if follow and player:
 		var direction = (player.global_position - global_position).normalized()
 		velocity = direction * speed
 	move_and_slide()
