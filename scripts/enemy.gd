c7c2480e9343af79316fc66a1622389e9e740be6
Ridji <andrijadimitrijevic2010@gmail.com>
2026-04-12 21:46:41 +0200

    main

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 var player: Node2D
 var speed: float = 100.0
-var hp = 20
+var hp = 5
 
-var follow: = true
+var follow = true
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -18,7 +18,10 @@ func _physics_process(delta: float) -> void:
 		var direction = (player.global_position-global_position).normalized()
 		velocity = lerp(velocity, direction * speed, 60*delta)
 	move_and_slide()
+	
+	if hp == 0:
+		queue_free()
 
-func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("bullets"):
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
 		hp = hp-1
